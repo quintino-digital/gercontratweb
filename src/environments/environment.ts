0c923f6cd0c8e05464377959c3561399cff4fd7b
Dysprosium <dysprosium@outlook.com.br>
2022-11-03 16:47:21 -0300

    GERCONTRAT0WEB03112022163504FEATURE

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,11 @@
 export const environment = {
   production: false,
-  endpoint_pessoa: "http://127.0.0.1:8080/api/v1/pessoa",
-  endpoint_tipo_contrato: "http://127.0.0.1:8080/api/v1/tipo-contrato",
+  endpoint_pessoa_local: "http://127.0.0.1:8080/api/v1/pessoa",
+  endpoint_pessoa: "http://gercontratapi.herokuapp.com/api/v1/pessoa",
+  endpoint_tipo_contrato_local: "http://127.0.0.1:8080/api/v1/tipo-contrato",
+  endpoint_tipo_contrato: "http://gercontratapi.herokuapp.com/api/v1/tipo-contrato",
+  endpoint_tipo_periodo_financeiro_local: "http://gercontratapi.herokuapp.com/api/v1/tipo-periodo-financeiro",
   endpoint_tipo_periodo_financeiro: "http://127.0.0.1:8080/api/v1/tipo-periodo-financeiro",
-  endpoint_contrato: "http://127.0.0.1:8080/api/v1/contrato",
+  endpoint_contrato_local: "http://127.0.0.1:8080/api/v1/contrato",
+  endpoint_contrato: "http://gercontratapi.herokuapp.com/api/v1/contrato",
 };
